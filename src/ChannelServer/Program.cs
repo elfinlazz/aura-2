85a9d71acbebb770f03a854fb2a393fabfc5159d
exec <exec@mabinoger.de>
2013-10-18 18:10:15 +0200

    Added project files

---
+++ src/ChannelServer/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence file in the main folder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aura.ChannelServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
