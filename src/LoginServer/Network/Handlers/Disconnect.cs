56db88e223ac83472d175cc784f9503f8092c6f5
exec <exec@mabinoger.de>
2013-11-01 03:19:07 +0100

    Added disconnect handler

---
+++ src/LoginServer/Network/Handlers/Disconnect.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence file in the main folder
+
+using Aura.Login.Database;
+using Aura.Shared.Network;
+using Aura.Shared.Util;
+
+namespace Aura.Login.Network.Handlers
+{
+	public partial class LoginServerHandlers : PacketHandlerManager<LoginClient>
+	{
+		/// <summary>
+		/// Sent before going back to login screen or continuing to a channel.
+		/// </summary>
+		/// <remarks>
+		/// No response, it only informs us that the connection is being closed.
+		/// </remarks>
+		/// <example>
+		/// 0001 [................] String : admin
+		/// 0002 [4D80902C6DF00000] Long   : ...
+		/// 0003 [........800000EE] Int    : 2147483886
+		/// 0004 [........00000000] Int    : 0
+		/// 0005 [........00000000] Int    : 0
+		/// </example>
+		[PacketHandler(Op.Disconnect)]
+		public void Disconnect(LoginClient client, MabiPacket packet)
+		{
+			var accountName = packet.GetString();
+			var unkLong = packet.GetLong();
+			var unkInt1 = packet.GetInt();
+			var unkInt2 = packet.GetInt();
+			var unkInt3 = packet.GetInt();
+
+			if (accountName != client.Account.Name)
+				return;
+
+			client.Account.LoggedIn = false;
+			LoginDb.Instance.UpdateAccount(client.Account);
+
+			Log.Info("'{0}' is closing the connection.", accountName);
+		}
+	}
+}
