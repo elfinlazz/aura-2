f3fd1f419b3a5e345246540defdc228acad2ff1e
exec <exec@mabinoger.de>
2014-02-26 11:01:23 +0100

    Updated knock back stun time
    Apparently all the same in the new combat system

--- src/Shared/Mabi/Const/Combat.cs
+++ src/Shared/Mabi/Const/Combat.cs
@@ -188,6 +188,6 @@ namespace Aura.Shared.Mabi.Const
 	public enum CombatStunAttacker { VeryFast = 450, Fast = 520, Normal = 600, Slow = 800, VerySlow = 1000 }
 	public enum CombatStunTarget { VeryFast = 1200, Fast = 1700, Normal = 2000, Slow = 2800, VerySlow = 3000 }
 
-	public enum CombatKnockbackStunAttacker { VeryFast = 1200, Fast = 2500, Normal = 2500, Slow = 2500, VerySlow = 2500 }
-	public enum CombatKnockbackStunTarget { VeryFast = 1500, Fast = 2000, Normal = 2000, Slow = 2800, VerySlow = 3000 }
+	public enum CombatKnockbackStunAttacker { VeryFast = 2500, Fast = 2500, Normal = 2500, Slow = 2500, VerySlow = 2500 }
+	public enum CombatKnockbackStunTarget { VeryFast = 3000, Fast = 3000, Normal = 3000, Slow = 3000, VerySlow = 3000 }
 }
