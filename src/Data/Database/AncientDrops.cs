9cfd3b1c6231342e7111a26ad9225d0f0abe6bed
exec <exec@mabinoger.de>
2014-05-30 11:25:58 +0200

    Changed ancient drops db to use %
    instead of a 0-1 float

--- src/Data/Database/AncientDrops.cs
+++ src/Data/Database/AncientDrops.cs
@@ -13,6 +13,12 @@ namespace Aura.Data.Database
 			info.ItemId = entry.ReadInt(0);
 			info.Chance = entry.ReadFloat(1);
 
+			info.Chance /= 100;
+			if (info.Chance > 1)
+				info.Chance = 1;
+			else if (info.Chance < 0)
+				info.Chance = 0;
+
 			this.Entries.Add(info);
 		}
 	}
