9dd59bbd86620a19a68cde91485db9f115335f63
exec <exec@mabinoger.de>
2014-07-16 14:44:29 +0200

    Added Defense skill handler

---
+++ src/ChannelServer/Skills/Base/StandardPrepareHandler.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Aura.Channel.Network.Sending;
+using Aura.Channel.World.Entities;
+using Aura.Shared.Network;
+using System;
+
+namespace Aura.Channel.Skills.Base
+{
+	public class StandardPrepareHandler : IPreparable, IReadyable, ICompletable, ICancelable
+	{
+		public virtual void Prepare(Creature creature, Skill skill, int castTime, Packet packet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void Ready(Creature creature, Skill skill, Packet packet)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void Complete(Creature creature, Skill skill, Packet packet)
+		{
+			Send.SkillComplete(creature, skill.Info.Id);
+		}
+
+		public virtual void Cancel(Creature creature, Skill skill)
+		{
+		}
+	}
+}
---
+++ src/ChannelServer/Skills/Combat/Defense.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using Aura.Channel.Network.Sending;
+using Aura.Channel.Skills.Base;
+using Aura.Channel.World.Entities;
+using Aura.Shared.Mabi.Const;
+using Aura.Shared.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aura.Channel.Skills.Combat
+{
+	/// <summary>
+	/// Handle for the Defense skill.
+	/// </summary>
+	[Skill(SkillId.Defense)]
+	public class Defense : StandardPrepareHandler
+	{
+		public override void Prepare(Creature creature, Skill skill, int castTime, Packet packet)
+		{
+			Send.SkillFlashEffect(creature);
+			Send.SkillPrepare(creature, skill.Info.Id, castTime);
+
+			creature.Skills.ActiveSkill = skill;
+		}
+
+		public override void Ready(Creature creature, Skill skill, Packet packet)
+		{
+			Send.SkillReady(creature, skill.Info.Id);
+		}
+	}
+}
