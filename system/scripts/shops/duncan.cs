b6fbba235f53030377159538e537ab959343cde2
Michael Kirkpatrick <mlkirkpatrick@olivet.edu>
2014-07-27 23:03:32 -0400

    -Added log when trying to access a shop that doesn't exist. -Added Malcolm, but he looks like Ranald for now because I haven't figured out how to get the items NPCs have equipped quickly and accurately. -Added Duncan's shop, but only the book in the Etc tab for now because the quest scrolls are all named the same which makes things rather confusing. -Updated/Improved Duncan and Nao's text, including beginner tutorial quest. I created a new character in the current NA version and copied this text directly from the game, so apparently much of this text has changed since these scripts were made.

---
+++ system/scripts/shops/duncan.cs
@@ -0,0 +1,17 @@
+//--- Aura Script -----------------------------------------------------------
+// Duncan's unique shop
+//--- Description -----------------------------------------------------------
+// Sells quest scrolls
+//---------------------------------------------------------------------------
+
+public class DuncanShop : NpcShopScript
+{
+	public override void Setup()
+	{
+        //Quest tab
+
+        //Party Quest tab
+
+        Add("Etc", 1045);          // Hit What You See
+	}
+}
