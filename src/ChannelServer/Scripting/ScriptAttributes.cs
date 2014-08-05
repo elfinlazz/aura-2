43b85e29fdd69c8b3c4f67f552c217c0a8344d33
exec <exec@mabinoger.de>
2014-08-05 16:08:35 +0200

    Added script overriding and removing

---
+++ src/ChannelServer/Scripting/ScriptAttributes.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+
+namespace Aura.Channel.Scripting
+{
+	public class OverrideAttribute : Attribute
+	{
+		public string TypeName { get; private set; }
+
+		public OverrideAttribute(string typeName)
+		{
+			this.TypeName = typeName;
+		}
+	}
+
+	public class RemoveAttribute : Attribute
+	{
+		public string[] TypeNames { get; private set; }
+
+		public RemoveAttribute(params string[] typeNames)
+		{
+			this.TypeNames = typeNames;
+		}
+	}
+}
