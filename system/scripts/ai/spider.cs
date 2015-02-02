467749fc945535e01fb0faa537c7c6f711704bd2
exec <exec@mabinoger.de>
2015-02-02 17:43:45 +0100

    Added Spider AI

---
+++ system/scripts/ai/spider.cs
@@ -0,0 +1,40 @@
+//--- Aura Script -----------------------------------------------------------
+//  Spider AI
+//--- Description -----------------------------------------------------------
+//  AI for normal spiders.
+//---------------------------------------------------------------------------
+
+public class SpiderAi : AiScript
+{
+	public SpiderAi()
+	{
+		Doubts("/pc/", "/pet/");
+	}
+
+	protected override IEnumerable Idle()
+	{
+		Do(Wander());
+		if(Random() < 25)
+			Do(PrepareSkill(SkillId.WebSpinning));
+		Do(Wait(2000, 5000));
+	}
+	
+	protected override IEnumerable Alert()
+	{
+		Do(Follow(400, true));
+	}
+	
+	protected override IEnumerable Aggro()
+	{
+		if(Random() < 50)
+		{
+			Do(PrepareSkill(SkillId.Defense));
+			Do(Circle(400, 1000, 5000));
+			Do(CancelSkill());
+		}
+		else
+		{
+			Do(Attack());
+		}
+	}
+}
