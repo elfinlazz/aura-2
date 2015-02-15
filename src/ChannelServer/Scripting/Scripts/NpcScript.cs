701865e773a924b8d7a2afd5938db76df76b74ba
exec <exec@mabinoger.de>
2015-02-15 16:09:52 +0100

    Fixed max durability upgrade effect

--- src/ChannelServer/Scripting/Scripts/NpcScript.cs
+++ src/ChannelServer/Scripting/Scripts/NpcScript.cs
@@ -1243,7 +1243,7 @@ namespace Aura.Channel.Scripting.Scripts
 					case "AttackRange": result.Item.OptionInfo.EffectiveRange += (short)effect.Value[0]; break;
 
 					case "MaxDurability":
-						result.Item.OptionInfo.DurabilityMax += (byte)effect.Value[0];
+						result.Item.OptionInfo.DurabilityMax += effect.Value[0] * 1000;
 						if (result.Item.OptionInfo.DurabilityMax < result.Item.OptionInfo.Durability)
 							result.Item.OptionInfo.Durability = result.Item.OptionInfo.DurabilityMax;
 						break;
