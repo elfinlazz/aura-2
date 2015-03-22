11e2383991f9d7a358f15a37d1876aed3e6e3017
exec <exec@mabinoger.de>
2015-03-22 14:20:14 +0100

    Fixed typos in ogre hunting quest

---
+++ sql/update_2015-03-22.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_progress` SET `objective` = "kill_ogre" WHERE `objective` = "kill_orge";
--- system/scripts/quests/beginner_uladh/hunting/1000053_hunting_ogre.cs
+++ system/scripts/quests/beginner_uladh/hunting/1000053_hunting_ogre.cs
@@ -1,22 +1,22 @@
 //--- Aura Script -----------------------------------------------------------
-// Hunt 1 Orge
+// Hunt 1 Ogre
 //--- Description -----------------------------------------------------------
 // Twentieth  hunting quest beginner quest series, started automatically
 // after completing Hunt 1 Troll or Reaching Level 60.
 //---------------------------------------------------------------------------
 
-public class OrgeQuestScript : QuestScript
+public class OgreQuestScript : QuestScript
 {
 	public override void Load()
 	{
 		SetId(1000053);
-		SetName("Hunt 1 Orge");
+		SetName("Hunt 1 Ogre");
 		SetDescription("The Ogre that has destructive power flowing out of its large figure, is the boss of the Fomors. It's not easy to face an ogre but please hunt the ogre. - Eavan -");
 		
 		SetReceive(Receive.Automatically);
 		AddPrerequisite(Or(Completed(1000052), ReachedTotalLevel(60)));
 		
-		AddObjective("kill_orge", "Hunt 1 Orge", 30, 19958, 67791, Kill(1, "/orge/"));
+		AddObjective("kill_ogre", "Hunt 1 Ogre", 30, 19958, 67791, Kill(1, "/ogre/"));
 
 		AddReward(Exp(15000));
 		AddReward(Item(40305, 1));
