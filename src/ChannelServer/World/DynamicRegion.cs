de863147308a6f8cd933903883c96b1ddc8c2404
exec <exec@mabinoger.de>
2015-04-20 14:11:00 +0200

    Fixed area ids for dynamic regions

--- src/ChannelServer/World/DynamicRegion.cs
+++ src/ChannelServer/World/DynamicRegion.cs
@@ -32,5 +32,26 @@ namespace Aura.Channel.World
 
 			ChannelServer.Instance.World.DynamicRegions.Add(this);
 		}
+
+		/// <summary>
+		/// Creates list of areas.
+		/// </summary>
+		/// <remarks>
+		/// Areas in the client files aren't in order, it can be id 1, 2, 3,
+		/// but it can just as well be 2, 1, 3. When the client creates a dynamic
+		/// region it changes the ids to be order, so 2, 1, 3 would become
+		/// 1, 2, 3 in the dynamic version. We have to mimic this to get the
+		/// correct ids for the props. Genius system, thanks, devCAT.
+		/// </remarks>
+		protected override void LoadAreas()
+		{
+			base.LoadAreas();
+
+			var id = 1;
+
+			lock (_areas)
+				foreach (var area in _areas)
+					area.Id = id++;
+		}
 	}
 }
