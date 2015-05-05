c3e79e96e3ccfe55a66904234f989d052a9f2fd6
exec <exec@mabinoger.de>
2015-05-05 20:18:19 +0200

    Added scripting base classes to Shared

---
+++ src/Shared/Scripting/Compilers/CompilerErrors.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aura.Shared.Scripting.Compilers
+{
+	public class CompilerErrorsException : Exception
+	{
+		public List<CompilerError> Errors { get; protected set; }
+
+		public CompilerErrorsException()
+		{
+			this.Errors = new List<CompilerError>();
+		}
+	}
+
+	public class CompilerError : Exception
+	{
+		public string File { get; protected set; }
+		public int Line { get; protected set; }
+		public int Column { get; protected set; }
+		public bool IsWarning { get; protected set; }
+
+		public CompilerError(string file, int line, int column, string message, bool isWarning)
+			: base(message)
+		{
+			this.File = file;
+			this.Line = line;
+			this.Column = column;
+			this.IsWarning = isWarning;
+		}
+	}
+}
