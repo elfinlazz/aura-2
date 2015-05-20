d57a2b934439b4d00946834c571b8d10f8967b35
exec <exec@mabinoger.de>
2015-05-20 12:58:18 +0200

    Fixed general_scripting merge errors
    Weird.

--- src/ChannelServer/Scripting/Scripts/ItemScript.cs
+++ src/ChannelServer/Scripting/Scripts/ItemScript.cs
@@ -5,6 +5,7 @@ using Aura.Channel.Network.Sending;
 using Aura.Channel.World.Entities;
 using Aura.Data;
 using Aura.Mabi;
+using Aura.Mabi.Const;
 using Aura.Shared.Util;
 using System;
 using System.Collections.Generic;
---
+++ src/Shared/Scripting/Scripts/IScript.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+namespace Aura.Shared.Scripting.Scripts
+{
+	public interface IScript
+	{
+		bool Init();
+	}
+
+	public interface IAutoLoader
+	{
+		void AutoLoad();
+	}
+}
