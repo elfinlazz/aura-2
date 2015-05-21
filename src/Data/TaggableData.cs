c033079a3e6d185e233276dba41aeb75525a2d1a
exec <exec@mabinoger.de>
2015-05-22 00:04:38 +0200

    Added more documentation for HasTag

--- src/Data/TaggableData.cs
+++ src/Data/TaggableData.cs
@@ -16,6 +16,18 @@ namespace Aura.Data
 		/// </summary>
 		/// <remarks>
 		/// Use asterisks (*) as place-holders.
+		/// 
+		/// Judging by how the tags are used in any place I could find
+		/// I don't think devCAT actually parses them, but uses regex.
+		/// The only thing in official tags that can't be resolved
+		/// using regex is "&", which means you have to split the string,
+		/// but that's it. 
+		/// 
+		/// And: */equip/* & */armor/* & */cloth/*
+		/// Or: /equip/|/cloth/
+		/// 
+		/// I haven't seen a combination of the two, although it would work,
+		/// as long as it doesn't get too complex.
 		/// </remarks>
 		/// <example>
 		/// Tags: /animal/beast/blackleopard/goodnpc_no_attack_pc/activate_signal/revivebyrp/heal/pc/neutral/unable_tame/unable_bubble/unable_unsummon/no_finish/
@@ -26,24 +38,28 @@ namespace Aura.Data
 		/// Input: */blackleopard/*
 		/// Result: True
 		/// 
-		/// Input: */animal/*
+		/// Input: /animal/
 		/// Result: True
 		/// 
 		/// Input: */bear/*
 		/// Result: False
 		/// 
-		/// And: */equip/* & */armor/* & */cloth/*
+		/// Input: /animal/|/blackleopard/
+		/// Result: True
+		/// 
+		/// Input: /animal/&/unable_bubble/
+		/// Result: True
 		/// </example>
 		/// <param name="tag"></param>
 		/// <returns></returns>
 		public bool HasTag(string tag)
 		{
-			// TODO: Or? Groups?
+			tag = tag.Replace("*", ".*");
 
-			var tags = tag.Split('&'); // */equip/* & */armor/* & */cloth/*
+			var tags = tag.Split('&');
 			foreach (var t in tags)
 			{
-				var check = t.Trim().Replace("*", ".*");
+				var check = t.Trim();
 				if (!Regex.IsMatch(this.Tags, check))
 					return false;
 			}
