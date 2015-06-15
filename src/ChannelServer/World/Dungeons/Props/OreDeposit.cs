e65d1cf4c14bc3d1e44163e0436e26a1264e8a07
exec <exec@mabinoger.de>
2015-06-15 02:49:33 +0200

    Added puzzle collectprop_herb
    Still random placement.

---
+++ src/ChannelServer/World/Dungeons/Props/OreDeposit.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see license file in the main folder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aura.Channel.World.Dungeons.Props
+{
+	public class OreDeposit : DungeonProp
+	{
+		public OreDeposit(int propId, string name)
+			: base(propId, name, "normal")
+		{
+		}
+	}
+}
---
+++ system/scripts/dungeons/puzzles/collectprop_test.cs
@@ -0,0 +1,35 @@
+//--- Aura Script -----------------------------------------------------------
+// Ore Deposit Room
+//--- Description -----------------------------------------------------------
+// Spawns ore deposits in a room.
+//---------------------------------------------------------------------------
+
+using Aura.Channel.Scripting.Scripts;
+using Aura.Channel.World.Dungeons.Props;
+using Aura.Channel.World.Dungeons.Puzzles;
+using Aura.Channel.World.Entities;
+
+[PuzzleScript("collectprop_test")]
+public class CollectPropTestScript : PuzzleScript
+{
+	public override void OnPrepare(Puzzle puzzle)
+	{
+		var propPlace = puzzle.NewPlace("PropPlace");
+		propPlace.ReservePlace();
+		propPlace.ReserveDoors();
+	}
+
+	public override void OnPuzzleCreate(Puzzle puzzle)
+	{
+		var propPlace = puzzle.GetPlace("PropPlace");
+
+		int propId;
+		uint color;
+
+		for (int i = 1; i <= 3; ++i)
+		{
+			var oreDeposit = new OreDeposit(22000, "Deposit" + i);
+			propPlace.AddProp(oreDeposit, Placement.Random);
+		}
+	}
+}
