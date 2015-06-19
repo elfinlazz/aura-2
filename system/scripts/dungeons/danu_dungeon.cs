53b38b51d2e4339134c4003abf2668d3615ae7a2
exec <exec@mabinoger.de>
2015-06-19 16:42:28 +0200

    Added Fiodh Int routing

--- system/scripts/dungeons/danu_dungeon.cs
+++ system/scripts/dungeons/danu_dungeon.cs
@@ -5,6 +5,29 @@ public class FiodhDungeonRouteScript : DungeonScript
 {
 	public override bool Route(Creature creature, Item item, ref string dungeonName)
 	{
+		// Fiodh Int 1
+		if (item.Info.Id == 63119) // Fiodh Intermediate Fomor Pass for One
+		{
+			dungeonName = "gairech_fiodh_middle_1_dungeon";
+			return true;
+		}
+
+		// Fiodh Int 2
+		// TODO: Party check
+		if (item.Info.Id == 63120) // Fiodh Intermediate Fomor Pass for Two
+		{
+			dungeonName = "gairech_fiodh_middle_2_dungeon";
+			return true;
+		}
+
+		// Fiodh Int 4
+		// TODO: Party check
+		if (item.Info.Id == 63121) // Fiodh Intermediate Fomor Pass for Four
+		{
+			dungeonName = "gairech_fiodh_middle_4_dungeon";
+			return true;
+		}
+
 		dungeonName = "gairech_fiodh_dungeon";
 		return true;
 	}
