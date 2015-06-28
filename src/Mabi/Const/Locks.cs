e2161c621956b7f231971e63a0d611561a8d2bb0
exec <exec@mabinoger.de>
2015-06-28 13:03:01 +0200

    Updated Locks enum

--- src/Mabi/Const/Locks.cs
+++ src/Mabi/Const/Locks.cs
@@ -6,38 +6,124 @@ using System;
 namespace Aura.Mabi.Const
 {
 	/// <summary>
-	/// This list is based on vague information found in the client,
-	/// aside from the values at the end. Testing needed.
+	/// Locks to disable certain actions.
 	/// </summary>
+	/// <remarks>
+	/// This list is based on vague information found in the client
+	/// and some basic testing. Needs more research for unknown
+	/// and missing values.
+	/// </remarks>
 	[Flags]
 	public enum Locks : uint
 	{
-		MoveRun = 0x00000001,
-		MoveWalk = 0x00000002,
-		MoveAll = 0x00000003,
+		// ? = 0x01,
 
-		SkillAll = 0x00000040,
-		// ...
+		// ? = 0x02,
 
-		Summon = 0x00020000,
-		Ride = 0x00040000,
+		/// <summary>
+		/// Unable to change stance.
+		/// </summary>
+		/// <remarks>
+		/// Since you can't change your stance you can't attack or hit
+		/// props either.
+		/// Was 0x02 in older clients?
+		/// </remarks>
+		Stance = 0x04,
 
-		AviationTakeoff = 0x00100000,
-		AviationLand = 0x00200000,
-		AviationAll = 0x00F00000,
+		/// <summary>
+		/// Prevents walking entirely.
+		/// </summary>
+		Walk = 0x08,
 
-		PvpShowdown = 0x01000000,
-		PvpAll = 0x0F000000,
+		/// <summary>
+		/// Prevents running, when walking is possible the client does that instead.
+		/// </summary>
+		Run = 0x10,
 
-		ChatSend = 0x00000F00,
-		ChatRecv = 0x0000F000,
-		// &
-		ChatNormal = 0x00001100,
-		ChatParty = 0x00002200,
-		ChatWhipser = 0x00004400,
-		ChatAll = 0x0000FF00,
+		// ? = 0x20, (hit? attack and prop hit works)
+
+		/// <summary>
+		/// Prevents changing equipment in any way.
+		/// </summary>
+		ChangeEquipment = 0x40,
+
+		/// <summary>
+		/// Prevents using items.
+		/// </summary>
+		UseItem = 0x80,
+
+		// ? = 0x100, (usemagic? doesn't stop magic skills)
+
+		/// <summary>
+		/// Prevents preparing skills.
+		/// </summary>
+		/// <remarks>
+		/// Does not affect Start or attacking (Combat Mastery).
+		/// </remarks>
+		PrepareSkills = 0x200,
+
+		/// <summary>
+		/// Prevents attacking and hitting props.
+		/// </summary>
+		/// <remarks>
+		/// Does not affect stance changing, and character still runs
+		/// towards the target (not for props).
+		/// </remarks>
+		Attack = 0x400,
+
+		/// <summary>
+		/// Prevents picking up from and dropping items to the floor.
+		/// </summary>
+		/// <remarks>
+		/// Trying to drop something shows the message "Unable to discard item!".
+		/// </remarks>
+		PickUpAndDrop = 0x800,
+
+		/// <summary>
+		/// Prevents talking to NPCs.
+		/// </summary>
+		/// <remarks>
+		/// Character still runs to the NPC.
+		/// </remarks>
+		TalkToNpc = 0x1000,
+
+		// ? = 0x2000, (trade? )
+
+		/// <summary>
+		/// Prevents chat messages from the character.
+		/// </summary>
+		/// <remarks>
+		/// As a result it also prevents GM commands.
+		/// </remarks>
+		Speak = 0x4000,
+
+		// ? = 0x8000,
+
+		/// <summary>
+		/// Disables gesture buttons.
+		/// </summary>
+		/// <remarks>
+		/// While this disables the gesture action buttons, you can still
+		/// use them. For example, if they're in your hotkey bar.
+		/// </remarks>
+		Gesture = 0x10000,
+
+		/// <summary>
+		/// Prevents starting skills.
+		/// </summary>
+		/// <remarks>
+		/// Does not affect Prepare or attacking.
+		/// </remarks>
+		StartSkills = 0x20000,
+
+		// ?
 
 		// ------------------------------------------------------------------
+
+		/// <summary>
+		/// Prevents running and walking.
+		/// </summary>
+		Move = Run | Walk,
 
 		/// <summary>
 		/// Chat, Moving, [...] (0xEFFFFFFE)
