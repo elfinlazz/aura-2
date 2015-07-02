0b8acf1b389d77437a8334f5260c0a90326f8e32
exec <exec@mabinoger.de>
2015-07-02 02:04:41 +0200

    Fixed Defense cd not being reset in old cbs

--- src/ChannelServer/Skills/Combat/Defense.cs
+++ src/ChannelServer/Skills/Combat/Defense.cs
@@ -136,6 +136,25 @@ namespace Aura.Channel.Skills.Combat
 		}
 
 		/// <summary>
+		/// Resets the skill's cooldown in old combat.
+		/// </summary>
+		/// <remarks>
+		/// Defense doesn't use the new cooldown system, but Vars, similar
+		/// to Final Hit. Var7 seems to be the cooldown. That's why we have to
+		/// reset it here.
+		/// </remarks>
+		/// <param name="creature"></param>
+		/// <param name="skill"></param>
+		/// <param name="packet"></param>
+		public override void Complete(Creature creature, Skill skill, Packet packet)
+		{
+			base.Complete(creature, skill, packet);
+
+			if (!AuraData.FeaturesDb.IsEnabled("CombatSystemRenewal"))
+				Send.ResetCooldown(creature, skill.Info.Id);
+		}
+
+		/// <summary>
 		/// Cancels special effects.
 		/// </summary>
 		/// <param name="creature"></param>
